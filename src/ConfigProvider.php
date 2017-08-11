12ad6d583262bb4116dcfcfd4b6d5f45b9b3b6ef
devgkz <devg@ya.ru>
2017-08-11 08:52:26 +0700

    Refactor container config for module.

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -13,42 +13,29 @@ use Bit55\Midcore\Middleware\ErrorHandler;
 class ConfigProvider
 {
     /**
-     * Return configuration for this component.
-     *
+     * Return configuration array.
      * @return array
      */
     public function __invoke()
     {
         return [
-            'dependencies' => $this->getDependencyConfig(),
-            
-            'containerConfigCallables' => [function ($container) {
-                $container->share(Middleware\ControllerHandlerMiddleware::class)->withArgument($container);
-                $container->share(Middleware\ActionHandlerMiddleware::class)->withArgument($container);
-                $container->share(Middleware\NotFoundHandler::class)->withArgument($container);
-            }]
-        ];
-    }
-
-    /**
-     * Return dependency mappings for this component.
-     * @return array
-     */
-    public function getDependencyConfig()
-    {
-        return [
-
-            'factories' => [
-                'templates'  => Factory\TemplaterFactory::class,
-                // ActionHandlerMiddleware::class => ActionHandlerMiddleware::class,
-                // ControllerHandlerMiddleware::class => ControllerHandlerMiddleware::class,
-                // NotFoundHandler::class => NotFoundHandler::class,
-            ],
-            'invokables' => [
-                ResponseEmitterInterface::class => DiactorosResponseEmitter::class,                
-                FastRouteMiddleware::class => FastRouteMiddleware::class,                
-                ErrorHandler::class => ErrorHandler::class
-            ],
+            'dependencies' => [
+                'factories' => [
+                    'templates'  => Factory\TemplaterFactory::class,
+                ],
+                'invokables' => [
+                    ResponseEmitterInterface::class => DiactorosResponseEmitter::class,
+                    FastRouteMiddleware::class => FastRouteMiddleware::class,
+                    ErrorHandler::class => ErrorHandler::class
+                ],
+                'callables' => [
+                    function ($container) {
+                        $container->share(Middleware\ControllerHandlerMiddleware::class)->withArgument($container);
+                        $container->share(Middleware\ActionHandlerMiddleware::class)->withArgument($container);
+                        $container->share(Middleware\NotFoundHandler::class)->withArgument($container);
+                    }
+                ]
+            ]
         ];
     }
 }
